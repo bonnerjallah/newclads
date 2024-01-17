dd7d082c8bcfb8fbfcaa6f8f857b6838c1747a01
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-01-16 20:02:41 -0500

    modified vite.config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 const isProduction = process.env.NODE_ENV === 'production';
 
-
 export default defineConfig({
   base: isProduction ? '/' : '/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Clearing manualChunks
+      },
+    },
+  },
+  esbuild: {
+    jsxInject: `import React from 'react';`, // Add this line
+  },
+});
+
